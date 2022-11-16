2b1d11ea1976559a8776c57583423f49cb0ec7fc
Andrew Shang <ashang1524@gmail.com>
2022-11-16 11:04:01 -0600

    add router spy object and redo beforeEach

--- liquid-prep-app/src/app/components/advice/advice.component.spec.ts
+++ liquid-prep-app/src/app/components/advice/advice.component.spec.ts
@@ -2,38 +2,66 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdviceComponent } from './advice.component';
 import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterTestingModule } from '@angular/router/testing'; 
 
-describe('AdviceComponent', () => {
+fdescribe('AdviceComponent', () => {
   let component: AdviceComponent;
   let fixture: ComponentFixture<AdviceComponent>;
-  let router: Router;
+  // let router: {navigate: jasmine.createSpy('navigate')};
 
-  beforeEach(async () => {
+  beforeEach(async () => { 
     await TestBed.configureTestingModule({
-      declarations: [ AdviceComponent ]
+      declarations: [ AdviceComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
-  });
 
+    // router = TestBed.inject(Router);
+  });
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(AdviceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
+    TestBed.inject(Router);
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ], 
+      providers: [ { provide: Router, useValue: Router } ]
   });
 
+  
+  });
+
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should have a title', () => {
-  //   const title = fixture.nativeElement.querySelector('h1');
-  //   expect(title.textContent).toContain('Advice');
-  // });
+  // check if the word plantingDays is in the html
+  it('should have a plantingDays', () => {
+    const fixture = TestBed.createComponent(AdviceComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.plantingDays').textContent).toContain('plantingDays');
+  });
 
-  it('should navigate by backClicked', () => {
+  it('should navigate by backClicked', () => { 
     const spy = spyOn(router, 'navigate');
     component.backClicked();
     expect(spy).toHaveBeenCalled();
   } );
+
+  it('should navigate by onFabClicked', () => {
+    const spy = spyOn(router, 'navigate');
+    component.onFabClicked();
+    expect(spy).toHaveBeenCalled();
+  } );
+
+  it(`should navigate to back`, () => {
+    component.backClicked();
+    expect (router.navigate).toHaveBeenCalled();
+ });
+
+  
 });
